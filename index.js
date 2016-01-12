6dd6c0a60f715d1901cd3b5f6ebd21287c8902ff
hieu292 <nguyen.huy.hieu292@gmail.com>
2016-01-12 18:08:14 +0700

    update task 2

--- index.js
+++ index.js
@@ -121,3 +121,34 @@ async.parallel({
         console.log(result);
     }
 });
+
+//example parallelLimit
+
+async.parallelLimit([
+    function (callback) {
+        setTimeout(function () {
+            console.log('function 1 called');
+            callback(null, 'one');
+        }, 3000);
+
+    },
+    function (callback) {
+        setTimeout(function () {
+            console.log('function 2 called');
+            callback(null, 'two');
+        }, 2000);
+    },
+    function (callback) {
+        setTimeout(function () {
+            console.log('function 3 called');
+            callback(null, 'three');
+        }, 1000);
+    }
+
+],2, function (err, result) {
+    if(err){
+        console.error(err);
+    }else{
+        console.log(result);
+    }
+});
--- temp.js
+++ temp.js
@@ -1,2 +1,30 @@
 var async = require('async');
 
+async.parallelLimit([
+    function (callback) {
+        setTimeout(function () {
+            console.log('function 1 called');
+            callback(null, 'one');
+        }, 3000);
+
+    },
+    function (callback) {
+        setTimeout(function () {
+            console.log('function 2 called');
+            callback(null, 'two');
+        }, 2000);
+    },
+    function (callback) {
+        setTimeout(function () {
+            console.log('function 3 called');
+            callback(null, 'three');
+        }, 1000);
+    }
+
+],2, function (err, result) {
+    if(err){
+        console.error(err);
+    }else{
+        console.log(result);
+    }
+});
